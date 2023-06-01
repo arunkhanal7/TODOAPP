2c22eceb0a66a12c4c8aa24ca9090713bdbcedf7
Arun <kingarun2020@gmail.com>
2023-06-01 16:31:44 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    title: "TODO APP",
+    home: TODOAPP(),
+  ));
+}
+
+class TODOAPP extends StatefulWidget {
+  const TODOAPP({super.key});
+
+  @override
+  State<TODOAPP> createState() => _TODOAPPState();
+}
+
+class _TODOAPPState extends State<TODOAPP> {
+  String value = '';
+  var listTODO = [''];
+  addTODO(String item) {
+    setState(() {
+      listTODO.add(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('TODO APP'),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              addTODO(value);
+            }),
+        body: ListView.builder(
+            itemCount: listTODO.length,
+            itemBuilder: (context, index) {
+              return index == 0
+                  ? Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(hintText: 'Add Item'),
+                        onChanged: (val) {
+                          value = val;
+                        },
+                      ),
+                    )
+                  : ListTile(
+                      leading: Icon(Icons.add),
+                      title: Text(
+                        '${listTODO[index]}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    );
+            }));
+  }
+}
